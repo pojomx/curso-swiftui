fd9bd6458922d8acc7b7b4aeacf1ed22cf8cf1d5
Alan Milke <alan.marquez@bairesdev.com>
2022-08-15 12:25:47 -0500

    Finished home view
     - Refactored part of the code for reusability

---
+++ Restart-App/Students/RestartApp/RestartApp/Views/CircleGroupView.swift
@@ -0,0 +1,35 @@
+//
+//  CircleGroupView.swift
+//  RestartApp
+//
+//  Created by Alan Milke on 15/08/22.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        }
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
