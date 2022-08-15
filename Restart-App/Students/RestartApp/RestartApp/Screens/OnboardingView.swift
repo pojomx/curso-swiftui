d24001d1e5e4c836de6e2049400784e6ae0a14e7
Alan Milke <alan.marquez@bairesdev.com>
2022-08-15 12:02:13 -0500

    Onboarding screen design ready

--- Restart-App/Students/RestartApp/RestartApp/Screens/OnboardingView.swift
+++ Restart-App/Students/RestartApp/RestartApp/Screens/OnboardingView.swift
@@ -12,13 +12,96 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
 
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack(spacing: 20) {
+                // MARK: - HEADER
+                Spacer()
+                VStack(spacing: 0) {
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                        It's not how mucho we give but
+                        how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } //: Header
+                // MARK: - CENTER
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Image("character-1")
+                            .resizable()
+                            .scaledToFit()
+                    } //: Center
+                } 
+                // MARK: - FOOTER
+                ZStack {
+                    // Parts of the custom button
+                    
+                    // 1. Background (Static)
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                        
+                    // 2. Call to Action (Static)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    // 3. Capsle (Dynamic width)
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    
+                    // 4. Circle (Draggable)
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                            
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        Spacer()
+                    }
+                    
+                    
+                } //: Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
+                
             }
         }
     }
