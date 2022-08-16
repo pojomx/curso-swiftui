a8e2c1428f008e11bcce000af9ac8288383fc1c6
Alan Milke <alan.marquez@bairesdev.com>
2022-08-16 10:34:47 -0500

    Added double tap gesture

--- Pinch App/Students/PinchApp/PinchApp/Screen/ContentView.swift
+++ Pinch App/Students/PinchApp/PinchApp/Screen/ContentView.swift
@@ -10,11 +10,48 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: - PROPERTY
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
     // MARK: - METHODS
     // MARK: - CONTENT
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                // MARK: - Page Image
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0) // Same as '.animation(.linear(duration: 1), value: isAnimating)'
+                    .scaleEffect(imageScale)
+                // MARK: - 1. Tap Gesture
+                    .onTapGesture (count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+                
+                
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear { 
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
+        } //: Navigation
+        .navigationViewStyle(.stack)
     }
 }
 
