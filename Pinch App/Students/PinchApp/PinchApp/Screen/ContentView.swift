3161e0ca041d07afa0e35123a2370d5c6dfdf1d1
Alan Milke <alan.marquez@bairesdev.com>
2022-08-16 10:21:16 -0500

    Project set up, conf and directories

---
+++ Pinch App/Students/PinchApp/PinchApp/Screen/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PinchApp
+//
+//  Created by Alan Milke on 16/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // MARK: - PROPERTY
+    // MARK: - METHODS
+    // MARK: - CONTENT
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
