4f8a593eb5b6835d2deb80f2b1d3a66495cfd64d
Alan Milke <alan.marquez@bairesdev.com>
2022-08-18 15:11:47 -0500

    Added list view with fruits on it.

--- SUI2-APP-FRUITS/Students/Fructus/Fructus/Screen/ContentView.swift
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/Screen/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Parameters
+    var fruits = fruitsData
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { item in 
+                    FruitRowView(fruit: item)
+                        .padding(.vertical, 4)
+                }
+            }   
+            .navigationTitle("Fruits")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
     }
 }
