8389db0fb766573e8aae227b89488d9f1960d173
Alan Milke <alan.marquez@bairesdev.com>
2022-08-18 08:30:04 -0500

    Added assets and initial config

---
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Alan Milke on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
