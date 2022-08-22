cafcfb02995332a696afead9d928e6a325af3cec
Alan Milke <alan.marquez@bairesdev.com>
2022-08-22 08:49:18 -0500

    Expanded settings view

--- SUI2-APP-FRUITS/Students/Fructus/Fructus/Screen/SettingsView.swift
+++ SUI2-APP-FRUITS/Students/Fructus/Fructus/Screen/SettingsView.swift
@@ -19,11 +19,41 @@ struct SettingsView: View {
                 VStack(spacing: 20) {
                     
                     // MARK: SECTION 1
-                    ExtractedView()
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+                    ) {
+                        Divider()
+                            .padding(.vertical, 4)
+                        HStack(alignment: .center, spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            
+                            Text("Most fruits are naturally low in fat, sodium and calories. None have cholesterol. Fruits are sources ofmany essential nutrients, including potassium, dietary fiber, vitamins, and much more. ")
+                                .font(.footnote)
+                        }
+                        
+                    }
                     
                     // MARK: SECTION 2
                     
                     // MARK: SECTION 3
+                    
+                    GroupBox(
+                        label:
+                            SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+                    ) {
+                        SettingsRowView(nameLabel: "Developer", contentLabel: "Alan M.")
+                        SettingsRowView(nameLabel: "Designer", contentLabel: "Robert Petras")
+                        SettingsRowView(nameLabel: "Compatibility", contentLabel: "iOS 14.0+")
+                        SettingsRowView(nameLabel: "Website", linkLabel: "pojoclan.com", linkDestination: "www.pojoclan.com")
+                        SettingsRowView(nameLabel: "Twitter", linkLabel: "@pojomx", linkDestination: "www.twitter.com/pojomx")
+                        SettingsRowView(nameLabel: "SwiftUI", contentLabel: "3.0")
+                        SettingsRowView(nameLabel: "Version", contentLabel: "1.0")
+                    }
                     
                 } //: VStack
                 .navigationTitle(Text("Settings"))
@@ -47,28 +77,5 @@ struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
             .preferredColorScheme(.dark)
-    }
-}
-
-struct ExtractedView: View {
-    var body: some View {
-        GroupBox(
-            label:
-                SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
-        ) {
-            Divider()
-                .padding(.vertical, 4)
-            HStack(alignment: .center, spacing: 10) {
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 80, height: 80)
-                    .cornerRadius(9)
-                
-                Text("Most fruits are naturally low in fat, sodium and calories. None have cholesterol. Fruits are sources ofmany essential nutrients, including potassium, dietary fiber, vitamins, and much more. ")
-                    .font(.footnote)
-            }
-            
-        }
     }
 }
