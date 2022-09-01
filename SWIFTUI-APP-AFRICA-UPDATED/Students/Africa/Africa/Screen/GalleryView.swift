e04facde7b43fdae0f252e456e9f51236b1904b5
Alan Milke <alan.marquez@bairesdev.com>
2022-09-01 11:22:52 -0500

    Added missing json files

--- SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/Screen/GalleryView.swift
+++ SWIFTUI-APP-AFRICA-UPDATED/Students/Africa/Africa/Screen/GalleryView.swift
@@ -7,10 +7,31 @@
 
 import SwiftUI
 
+// MARK: PROPERTIES
+let animals: [Animal] = Bundle.main.decode("animals.json")
+
+let gridLayout: [GridItem] = [
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+    GridItem(.flexible())
+]
+
 struct GalleryView: View {
     var body: some View {
         ScrollView(.vertical) {
-            Text("Gallery")
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                ForEach(animals) { item in 
+                    Image(item.image)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 1)
+                            
+                        )
+                }
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(
